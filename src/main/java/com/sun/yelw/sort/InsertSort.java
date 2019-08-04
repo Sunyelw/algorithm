3291949b87a6e6808426b288b8418dc00ff6fa1a
huangyang3 <huangyang3@asiainfo.com>
2019-08-04 17:15:36 +0800

    三种平均时间复杂度O(N^2)的排序算法： 1、冒泡排序及优化 2、插入排序（无序数组、有序数组， 遍历无序数组，在有序数组中确定位置） 3、选择排序（无序数组、有序数组， 每次在无序数组中找到最小的，放入有序数组末尾）

---
+++ src/main/java/com/sun/yelw/sort/InsertSort.java
@@ -0,0 +1,43 @@
+package com.sun.yelw.sort;
+
+import java.util.Arrays;
+
+/**
+ * 项目名称:   MyDemo
+ * 包:        com.example.demo.sort
+ * 类名称:     InsertSort
+ * 类描述:     插入排序
+ * 创建人:     huangyang
+ * 创建时间:   2018/12/26 20:15
+ */
+public class InsertSort {
+
+	public static void main(String[] args) {
+
+		int[] A = {2, 4, 6, 1, 7, 5};
+		doSort(A);
+	}
+
+    /**
+     * 时间: 最好 O(N) 最坏 O(N^2) 平均 O(N^2)
+     * 空间: O(1) 没用到额外空间, 是原地排序
+     * 稳定性: A[j] > tmp: 稳定排序; A[j] >= tmp: 非稳定排序
+     *
+     */
+	private static void doSort(int[] A) {
+		for (int i = 1; i < A.length; i++) {
+			int tmp = A[i];
+			int j = i - 1;
+			// Two Arrays
+			// 这里不能写 A[j] >= tmp 否则会变成不稳定排序算法
+			while (j > -1 && A[j] > tmp) {
+				A[j + 1] = A[j];
+				j--;
+			}
+			A[j + 1] = tmp;
+		}
+
+		System.out.println(Arrays.toString(A));
+	}
+
+}
